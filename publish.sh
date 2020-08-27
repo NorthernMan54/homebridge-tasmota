cbf87e2be834267a35ca40ee76bab6d3ee9f5ad2
NorthernMan54 <buyminivan-northern@yahoo.ca>
2020-08-27 11:11:31 -0400

    WIP

---
+++ publish.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+npm audit
+#if npm audit; then
+#  npm run-script document
+#  rm *orig* *toc\.*
+  git add .
+  npm version patch -m "$1" --force
+  npm publish --tag latest
+  git commit -m "$1"
+#  git push origin beta --tags
+  git push origin master --tags
+#else
+#  echo "Not publishing due to security vulnerabilites"
+#fi
