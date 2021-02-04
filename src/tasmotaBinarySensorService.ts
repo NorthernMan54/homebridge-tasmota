7fcdcc12b5c71f7b13cce6d6b9212e24e8873f8c
NorthernMan54 <buyminivan-northern@yahoo.ca>
2021-02-04 13:33:13 -0500

    Further fixes for #26

--- src/tasmotaBinarySensorService.ts
+++ src/tasmotaBinarySensorService.ts
@@ -110,13 +110,12 @@ export class tasmotaBinarySensorService extends TasmotaService {
       // debug('this.uniq_id', this.uniq_id);
       // debug('val_tpl', this.accessory.context.device[this.uniq_id].val_tpl);
       // debug('message', JSON.parse(message.toString()));
+      let value = message.toString();
 
-      if (this.parseValue(this.accessory.context.device[this.uniq_id].val_tpl)) {
-        const value = this.parseValue(this.accessory.context.device[this.uniq_id].val_tpl, {
+      if (this.accessory.context.device[this.uniq_id].val_tpl) {
+        value = this.parseValue(this.accessory.context.device[this.uniq_id].val_tpl, {
           value_json: JSON.parse(message.toString()),
         });
-      } else {
-        const value = message.toString();
       }
       // debug('value', value, typeof value);
       // debug('device_class', this.device_class);
